154bfd20e034e482cfcfaaaec6b040a7ce5d08b8
ebbalg <161567380+ebbalg@users.noreply.github.com>
2025-06-23 11:34:31 +0200

    feat(streamlit): embed document viewer
    - Create automatic navigation from page upload to document view page
    - Create layout for document view page

--- app/home_page.py
+++ app/home_page.py
@@ -1,7 +1,19 @@
 import streamlit as st
+import docx
+
+st.set_page_config(layout="centered")
 
 # Main page content
 st.markdown("# Upload file")
 st.sidebar.markdown("# Upload file")
 
-st.file_uploader("Upload a Word document", type =["docx"])
+data = st.file_uploader("Upload a Word document", type =["docx"])
+
+if data:
+    doc = docx.Document(data)
+    full_text = "\n".join([para.text for para in doc.paragraphs])
+
+    st.session_state["doc_text"]= full_text
+
+    
+    st.switch_page("page_2.py")
