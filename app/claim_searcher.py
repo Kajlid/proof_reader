7e5d8c436f0de7f76afad7c4ec133fd612f7a640
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-07-14 16:25:52 +0200

    feat(gui): gui changes related to fact checking and tonality adjustment
    - search claims in parallel with thread pool executor and include trusted domains to tavily
    - give title to page and incorporate back button
    - cache tonality feedback and improve prompt
    - stream fact check statements
    - test llama model

--- app/claim_searcher.py
+++ app/claim_searcher.py
@@ -1,5 +1,7 @@
 from langchain_tavily import TavilySearch
+from langchain_tavily import TavilyExtract
 import os
+from concurrent.futures import ThreadPoolExecutor
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,19 +11,42 @@ tavily_key = os.getenv("TAVILY_API_KEY")
 
 search_tool = TavilySearch(
     max_results=3,
+    include_domains=["wwf.se", "worldwildlife.org", "britannica.com", "nature.com"],
 )
 
 
-def search_claims(claims_list=list, search_tool=search_tool) -> list:
+def search_and_extract(query, search_tool, extract_tool):
+    search = search_tool.invoke({"query": query})
+    search_results = search["results"]
+    refined_results = []
+    for res in search_results:
+        url = res["url"]
+        extraction = extract_tool.invoke({"urls": [url]})
+        if isinstance(extraction, dict) and "results" in extraction:
+            raw_content = extraction["results"][0]["raw_content"]
+        else:
+            raw_content = str(extraction)
+
+        refined_results.append(
+            {"title": res["title"], "url": url, "content": raw_content}
+        )
+    return refined_results
+
+
+def search_claims(
+    claims_list, search_tool=search_tool, extract_tool=TavilyExtract()
+):  # search claims in parallel
     result_list = []
-
-    for claim_dict in claims_list:
-        claim = claim_dict["påstående"]
-        query = claim_dict["sökfråga"]
-
-        search = search_tool.invoke({"query": query})
-        search_results = search["results"]
-
-        result_list.append({"claim": claim, "results": search_results})
-
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = []
+        for claim_dict in claims_list:
+            query = claim_dict["sökfråga"]
+            futures.append(
+                executor.submit(search_and_extract, query, search_tool, extract_tool)
+            )
+        for claim_dict, future in zip(claims_list, futures):
+            refined_results = future.result()
+            result_list.append(
+                {"claim": claim_dict["påstående"], "results": refined_results}
+            )
     return result_list
--- app/page_2.py
+++ app/page_2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
+import re
 import google.generativeai as genai
 from langchain_core.output_parsers.json import JsonOutputParser
 from langchain_core.output_parsers import StrOutputParser
@@ -14,61 +15,146 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-2.0-flash-lite")
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", api_key=api_key)
+
 
 st.set_page_config(layout="wide")
 
-check1, check2 = st.columns([0.005, 0.04], gap="small")
-with check1:
-    fact_check = st.checkbox("Faktakontroll")
-with check2:
-    tone_check = st.checkbox("Tonalitet")
+st.markdown(
+    """
+    <style>
+        .block-container {
+            padding-top: 3rem;
+        }
+    </style>
+""",
+    unsafe_allow_html=True,
+)
 
+# Back button
+top_col1, _, _ = st.columns([0.1, 0.8, 0.1])
+with top_col1:
+    if st.button("Back"):
+        st.switch_page("home_page.py")
+
+
+st.markdown(
+    '<h1 style="text-align: center;"> Textgranskare </h1>', unsafe_allow_html=True
+)
+
+
+col1, col2 = st.columns([0.05, 0.04], gap="small")
+with col1:
+    selected_option = st.radio(
+        "Välj granskningstyp",
+        ["Faktakontroll", "Tonalitet"],
+        horizontal=True,
+        label_visibility="collapsed",
+    )
+
+
+@st.cache_data(show_spinner="Söker och hämtar relaterade källor...")
+def get_claim_search_output():
+    claim_extr_prompt = PromptTemplate.from_template("""
+            Du är en redaktörsassistent som arbetar med faktagranskning.
+
+            Gå igenom följande text och extrahera endast de meningar eller stycken som innehåller sakliga påståenden - alltså fakta som skulle kunna kontrolleras genom en internetsökning.
+
+            Gör följande:
+            1. Identifiera varje faktapåstående och kopiera det ordagrant.
+            2. Uteslut allt som är subjektivt, spekulativt, innehåller värderingar, eller inte går att verifiera via internet.
+            3. För varje påstående, formulera en fokuserad sökfråga (till exempel en Google-sökning) som kan användas för att kontrollera sanningshalten.
+            4. Lista resultatet i detta format:
+
+            [
+            {{
+                "påstående": "WWF bildades 1961 i Schweiz.",
+                "sökfråga": "WWF bildades 1961 Schweiz"
+            }}
+            ]
+
+            Text att analysera:
+            \"\"\"
+            {input_text}
+            \"\"\"
+            """)
+    extract_sources_chain = claim_extr_prompt | llm | JsonOutputParser()
+    response = extract_sources_chain.invoke({"input_text": text})
+
+    result_list = search_claims(response)
+
+    return result_list
+
+
+@st.cache_data(show_spinner="Genererar tonalitetsfeedback...")
+def get_tonality_feedback(text):
+    prompt = f"""Jag vill att du hjälper mig att justera tonaliteten i en text. Målet är att få texten att låta mer trovärdig och naturlig, utan att ändra dess innebörd eller fakta. Följ de här fyra stegen:
+
+    1.Identifiera formuleringar som behöver förtydligas, samt formuleringar som har ett dåligt flyt eller en för slapp ton. Leta efter ord eller uttryck som innehåller överdrifter eller personliga värderingar.
+    2.Förklara kort varför varje uttryck du identifierar är otydligt eller har dåligt flyt.
+    3.Föreslå en omskrivning som gör uttrycket mer sammanhängande eller trovärdigt. Innehållet och betydelsen ska behållas.
+    4.Presentera varje fall i exakt detta format:
+
+    **Original**: [originalformulering] \n
+    **Kommentar**: [kort förklaring till varför det är subjektivt] \n
+    **Omskrivning**: [neutral version] \n
+        
+                    
+    Här är texten: {text}"""
+
+    response = llm.invoke(prompt)
+    return response.content
 
 
 col1, col2 = st.columns([2, 1])
 
+feedback_text = ""
+
 with col1:
     with st.container(border=True):
         if "doc_text" in st.session_state:
-            st.text(st.session_state["doc_text"])
+            st.markdown(st.session_state["doc_text"])
 
 
 with col2:
     text = st.session_state["doc_text"]
-    feedback_text = ""
 
-    if fact_check:
+    # FAKTAKOLL
+    if selected_option == "Faktakontroll":
+        st.download_button(
+            "Ladda ned feedback", feedback_text, file_name="feedback.txt"
+        )
         st.markdown("### **Faktakontroll**")
-        claim_extr_prompt = PromptTemplate.from_template("""
-        Du är en redaktörsassistent som arbetar med faktagranskning.
 
-        Gå igenom följande text och extrahera endast de meningar eller stycken som innehåller sakliga påståenden - alltså fakta som skulle kunna kontrolleras genom en internetsökning.
+        with st.spinner("Söker och hämtar relaterade källor..."):
+            claim_extr_prompt = PromptTemplate.from_template("""
+            Du är en redaktörsassistent som arbetar med faktagranskning.
 
-        Gör följande:
-        1. Identifiera varje faktapåstående och kopiera det ordagrant.
-        2. Uteslut allt som är subjektivt, spekulativt, innehåller värderingar, eller inte går att verifiera via internet.
-        3. För varje påstående, formulera en fokuserad sökfråga (till exempel en Google-sökning) som kan användas för att kontrollera sanningshalten.
-        4. Lista resultatet i detta format:
+            Gå igenom följande text och extrahera endast de meningar eller stycken som innehåller sakliga påståenden - alltså fakta som skulle kunna kontrolleras genom en internetsökning.
 
-        [
-        {{
-            "påstående": "WWF bildades 1961 i Schweiz.",
-            "sökfråga": "WWF bildades 1961 Schweiz"
-        }}
-        ]
+            Gör följande:
+            1. Identifiera varje faktapåstående och kopiera det ordagrant.
+            2. Uteslut allt som är subjektivt, spekulativt, innehåller värderingar, eller inte går att verifiera via internet.
+            3. För varje påstående, formulera en fokuserad sökfråga (till exempel en Google-sökning) som kan användas för att kontrollera sanningshalten.
+            4. Lista resultatet i detta format:
 
-        Text att analysera:
-        \"\"\"
-        {input_text}
-        \"\"\"
-        """)
+            [
+            {{
+                "påstående": "WWF bildades 1961 i Schweiz.",
+                "sökfråga": "WWF bildades 1961 Schweiz"
+            }}
+            ]
 
-        extract_sources_chain = claim_extr_prompt | llm | JsonOutputParser()
-        response = extract_sources_chain.invoke({"input_text": text})
+            Text att analysera:
+            \"\"\"
+            {input_text}
+            \"\"\"
+            """)
 
-        result_list = search_claims(response)
+            extract_sources_chain = claim_extr_prompt | llm | JsonOutputParser()
+            response = extract_sources_chain.invoke({"input_text": text})
+
+            result_list = search_claims(response)
 
         for claim_with_source in result_list:
             claim = claim_with_source["claim"]
@@ -80,17 +166,49 @@ with col2:
 
             evidence = ""
 
+            summarize_prompt = PromptTemplate.from_template(
+                """Plocka ut 2-3 hela meningar från denna text: {content}. 
+                Utgå ifrån de delar av texten som aktivt svarar på detta påstående: {claim}, så andra orelaterade delar av texten bör ignoreras.
+                Om du hittar en exakt eller väldigt lik formulering som påståendet bör denna tas med.
+                Om direkta siffror nämns så bör du försöka hitta de exakta siffrorna i texten som hör ihop med formuleringen i påståendet.
+                Generera inte nytt innehåll, utan plocka ut de sammanhängande meningarna i texten som överensstämmer mest med ämnet. 
+                Skapa ingen punktlista, utan skriv bara meningarna efter varandra, utan citattecken. Ta inte med någon ytterligare förklaring utan skriv bara ut meningarna som de är.
+                Om du inte kan extrahera meningar, lämna då svaret som en tom sträng, utan kommentar.
+                """
+            )
+
             st.markdown("### Relaterade källor:")
             for source in search_results:
                 title = source["title"]
                 url = source["url"]
                 content = source["content"]
-                evidence += content
 
                 # Display title with a hyperlink to the URL
                 st.markdown(f"[🔗 {title}]({url})", unsafe_allow_html=True)
 
-                st.markdown(content)
+                create_content_chain = summarize_prompt | llm | StrOutputParser()
+
+                # new_content = create_content_chain.invoke(        # invoke
+                # {"content": content, "claim": claim}
+                # )
+
+                response_stream = create_content_chain.stream(
+                    {"content": content, "claim": claim}
+                )
+
+                # evidence += new_content
+
+                output = st.empty()  # Create an empty placeholder
+                tokens = ""
+
+                for chunk in response_stream:
+                    tokens += chunk
+                    output.markdown(tokens)
+
+                new_content = tokens
+                evidence += new_content
+
+                # st.markdown(new_content)
 
                 feedback_text += f"Källor: {title}\n{url}\n{content}\n\n"
 
@@ -101,13 +219,13 @@ with col2:
                 Stämmer påståendet utifrån den här informationen (från sökresultat) som ska användas som underlag: {evidence}? 
                 
                 Din uppgift är att bedöma om påståendet:
-                - **Stöds av källor***
-                - **Motsägs av källor**
+                - **Påståendet stöds av källor***
+                - **Påståendet motsägs av källor**
                 - **Osäkert, kan behövas undersökas närmare** (t.ex. om källorna är motstridiga eller inte direkt stödjer påståendet)
                 
                 Ange din slutsats med ett av dessa tre alternativ, följt av en kort motivering på högst en mening. Exempel:
 
-                Stöds av källor  \n
+                Påståendet stöds av källor  \n
                 Motivering: Påståendet bekräftas direkt av en eller fler källor."""
             )
 
@@ -118,25 +236,39 @@ with col2:
             st.markdown(f"### Slutsats:\n{response}")
             feedback_text += f"AI-slutsats:\n{response}"
 
-    if tone_check:
+    # TONALITETSKOLL
+    elif selected_option == "Tonalitet":
+        st.download_button(
+            "Ladda ned feedback", feedback_text, file_name="feedback.txt"
+        )
         st.markdown("### **Tonalitetskontroll**")
-        prompt = f"""Jag vill att du hjälper mig att justera tonaliteten i en text. Målet är att göra texten mer neutral, objektiv och saklig, utan att ändra dess innebörd eller fakta. Följ de här fyra stegen:
 
-        1.Identifiera formuleringar som är subjektiva eller värderande. Leta efter ord eller uttryck som innehåller åsikter, känslor, överdrifter eller personliga värderingar.
-        2.Förklara kort varför varje uttryck du identifierar är subjektivt.
-        3.Föreslå en omskrivning som gör uttrycket mer neutralt och objektivt. Innehållet och betydelsen ska behållas.
-        4.Presentera varje fall i exakt detta format, med en radbrytning mellan varje nytt exempel:
+        full_text = get_tonality_feedback(text)
 
-        **Original**: [originalformulering] \n
-        **Kommentar**: [kort förklaring till varför det är subjektivt] \n
-        **Omskrivning**: [neutral version] \n\n
-        
-                    
-        Här är texten: {text}"""
+        feedback_text += "### Tonalitetskontroll\n" + full_text + "\n\n"
 
-        response = model.generate_content(prompt)
-        st.markdown(response.text)
-        feedback_text += "### Tonalitetskontroll\n" + response.text + "\n\n"
+        if "tonality_blocks" not in st.session_state:
+            raw_blocks = re.split(r"\n(?=\*\*Original\*\*:)", full_text.strip())
+            st.session_state.tonality_blocks = raw_blocks
+            st.session_state.show_full_text = False
+
+        block_limit = 4
+
+        # Display either preview or full output
+
+        if st.session_state.show_full_text:
+            for block in st.session_state.tonality_blocks:
+                st.markdown(block.strip())
+                st.markdown("---")
+            if st.button("Visa mindre"):
+                st.session_state.show_full_text = False
+        else:
+            for block in st.session_state.tonality_blocks[:block_limit]:
+                st.markdown(block.strip())
+                st.markdown("---")
+
+            if st.button("Visa mer"):
+                st.session_state.show_full_text = True
 
     # Downloading the result file
-    st.download_button("Ladda ned feedback", feedback_text, file_name="feedback.txt")
+    # st.download_button("Ladda ned feedback", feedback_text, file_name="feedback.txt")
